db6ef421eaf145de92c48f4a4b3a424c829d46ee
hong <397956861@qq.com>
2024-01-27 11:08:37 +0800

    显示用户名和头像

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,8 +2,8 @@ const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
   token: state => state.user.token,
-  userId: state => state.user.userInfo.suerId,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name
+  userId: state => state.user.userInfo.suerId, // 用户ID
+  avatar: state => state.user.userInfo.staffPhoto, // 用户头像
+  name: state => state.user.userInfo.username // 用户名称
 }
 export default getters
