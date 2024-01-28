de4f76a5a9ff598ed8e1f9049a47d873560d9a30
hong <397956861@qq.com>
2024-01-28 20:55:55 +0800

    下拉菜单-负责人数据的获取

--- src/api/department.js
+++ src/api/department.js
@@ -1,8 +1,15 @@
 import request from '@/utils/request'
 
-// 获取部门列表数据
+// 获取组织架构-部门列表数据
 export const getDepartment = () => {
   return request({
     url: '/company/department'
+  })
+}
+
+// 获取-部门负责人列表
+export const getManagerList = () => {
+  return request({
+    url: '/sys/user/simple'
   })
 }
--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -8,7 +8,10 @@
         <el-input v-model="formData.code" style="width: 80%;" size="mini" placeholder="2-10个字符" />
       </el-form-item>
       <el-form-item label="部门负责人" prop="managerId">
-        <el-select v-model="formData.managerId" style="width: 80%;" size="mini" placeholder="请选择负责人" />
+        <el-select v-model="formData.managerId" style="width: 80%;" size="mini" placeholder="请选择负责人">
+          <!-- 下拉选项 -->
+          <el-option v-for="item in managerList" :key="item.id" :label="item.username" :value="item.id" />
+        </el-select>
       </el-form-item>
       <el-form-item label="部门介绍" prop="introduce">
         <el-input v-model="formData.introduce" type="textarea" :rows="4" style="width: 80%;" size="mini" placeholder="1-100个字符" />
@@ -26,7 +29,7 @@
 </template>
 
 <script>
-import { getDepartment } from '@/api/department'
+import { getDepartment, getManagerList } from '@/api/department'
 
 export default {
   props: {
@@ -41,14 +44,15 @@ export default {
   },
   data() {
     return {
-      formData: {
+      managerList: [], // 部门负责人列表
+      formData: { // 表单数据
         name: '',
         code: '',
         managerId: '',
         introduce: '',
         pid: ''
       },
-      rules: {
+      rules: { // 表单校验规则
         name: [
           { required: true, message: '部门名称不能为空', trigger: 'blur' },
           { min: 2, max: 10, message: '部门名称的长度为2-10个字符', trigger: 'blur' },
@@ -86,9 +90,17 @@ export default {
       }
     }
   },
+  created() {
+    this.getManagerList()
+  },
   methods: {
+    // 关闭弹层
     close() {
       this.$emit('update:show-dialog', false)
+    },
+    // 获取部门负责人列表
+    async getManagerList() {
+      this.managerList = await getManagerList()
     }
   }
 }
