06d53c558668ba330e64aa484a7328d55510c545
hong <397956861@qq.com>
2024-01-28 17:59:57 +0800

    添加子部门-弹层组件创建

---
+++ src/views/department/components/add-dept.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-dialog title="新增部门" :visible="showDialog" @close="close">
+    <el-form>
+      <el-form-item>
+        <el-input />
+      </el-form-item>
+      <el-form-item>
+        <el-input />
+      </el-form-item>
+      <el-form-item>
+        <el-input />
+      </el-form-item>
+      <el-form-item>
+        <el-input />
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    close() {
+      this.$emit('update:show-dialog', false)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="app-container">
       <!-- 展示树形结构 -->
-      <el-tree :data="depts" :props="defaultProps" default-expand-all>
+      <el-tree :expand-on-click-node="false" :data="depts" :props="defaultProps" default-expand-all>
         <!-- 节点结构，树形结构会重复渲染节点结构里面的内容  -->
         <!-- 通过作用域插槽，传入两个参数node和data，分别表示当前节点的 Node 对象和当前节点的数据。 -->
         <template v-slot="{ data }">
@@ -11,14 +11,14 @@
             <el-col :span="4">
               <span class="tree-manager">{{ data.managerName }}</span>
               <!-- 下拉菜单 -->
-              <el-dropdown>
+              <el-dropdown @command="operate">
                 <span class="el-dropdown-link">
                   操作<i class="el-icon-arrow-down el-icon--right" />
                 </span>
                 <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加权限</el-dropdown-item>
-                  <el-dropdown-item>编辑部门</el-dropdown-item>
-                  <el-dropdown-item>删除</el-dropdown-item>
+                  <el-dropdown-item command="add">添加权限</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
             </el-col>
@@ -26,26 +26,25 @@
         </template>
       </el-tree>
     </div>
+    <!-- .sync事件修饰符：自动绑定 @update: showDialog事件 -->
+    <!-- 子组件提交同名触发函数(update: showDialog),传递过来值自动赋予父组件绑定的变量 -->
+    <add-dept :show-dialog.sync="showDialog" />
   </div>
 </template>
 <script>
 import { getDepartment } from '@/api/department'
 import { transListToTreeData } from '@/utils'
+import addDept from './components/add-dept.vue'
 
 export default {
   name: 'Department',
+  components: {
+    addDept
+  },
   data() {
-    return { //  数据属性
-      depts: [],
-      // depts: [{
-      //   name: '传智教育', managerName: '管理员', children: [{
-      //     name: '总裁办', managerName: '张三'
-      //   }, {
-      //     name: '人事部', managerName: '李四'
-      //   }, {
-      //     name: '行政部', managerName: '王五'
-      //   }]
-      // }],
+    return {
+      showDialog: false,
+      depts: [], //  数据属性
       defaultProps: {
         label: 'name', // 显示的字段名称
         children: 'children' // 读取子节点的字段名称
@@ -62,6 +61,19 @@ export default {
       // console.log(res)
       // 调用封装好的递归函数，将列表型数据转换成树形结构的数据
       this.depts = transListToTreeData(res, 0)
+    },
+    operate(type) {
+      // console.log(type)
+      if (type === 'add') {
+        // 添加子部门
+        this.showDialog = true
+      }
+      if (type === 'edit') {
+        // 编辑子部门
+      }
+      if (type === 'del') {
+        // 删除子部门
+      }
     }
   }
 }
