6fa3796fda045d094ebe2f0d6f86cf9fef00250d
hong <397956861@qq.com>
2024-01-30 19:25:45 +0800

    表格列的自定义结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -7,9 +7,19 @@
       <el-table :data="list" :border="true">
         <!-- <el-table-column label="序号" align="center" /> -->
         <el-table-column prop="name" label="角色" align="center" width="200" />
-        <el-table-column prop="state" label="启用" align="center" width="200" />
+        <el-table-column prop="state" label="启用" align="center" width="200">
+          <template v-slot="{row}">
+            <span>{{ row.state === 1? '已启用': row.state === 0? '未启用' : '无' }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="description" label="描述" align="center" />
-        <el-table-column label="操作" align="center" />
+        <el-table-column label="操作" align="center">
+          <template>
+            <el-button type="text" size="mini">分配权限</el-button>
+            <el-button type="text" size="mini">编辑</el-button>
+            <el-button type="text" size="mini">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <el-row type="flex" justify="end" style="height: 60px;" align="middle">
         <el-pagination layout="prev, pager, next" />
