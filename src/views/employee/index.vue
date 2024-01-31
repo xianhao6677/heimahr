0bb78f47bc4e235a5f79f8cf8f2c8ef5ee2e5b17
hong <397956861@qq.com>
2024-01-31 16:54:19 +0800

    记录树的切换节点的id

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -4,6 +4,16 @@
       <div class="left">
         <el-input style="margin-bottom:10px" type="text" prefix-icon="el-icon-search" size="small" placeholder="输入员工姓名全员搜索" />
         <!-- 树形组件 -->
+        <el-tree
+          ref="deptTree"
+          node-key="id"
+          :data="depts"
+          :props="defaultProps"
+          :expand-on-click-node="false"
+          default-expand-all
+          highlight-current
+          @current-change="selsctNode"
+        />
       </div>
       <div class="right">
         <el-row class="opeate-tools" type="flex" justify="end">
@@ -19,8 +29,45 @@
 </template>
 
 <script>
+import { getDepartment } from '@/api/department'
+import { transListToTreeData } from '@/utils'
 export default {
-  name: 'Employee'
+  name: 'Employee',
+  data() {
+    return {
+      depts: [], // 部门数据
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      },
+      queryParams: { // 请求获取员工列表的查询参数
+        departmentId: null
+      }
+    }
+  },
+  created() {
+    this.getDepartment()
+  },
+  methods: {
+    //  获取部门列表数据
+    async getDepartment() {
+      const res = await getDepartment()
+      // 调用封装好的递归函数，将列表转换成树形数据
+      this.depts = transListToTreeData(res, 0)
+      // 获取根节点 id，加载页面时默认获取选中根节点，并获取对应数据
+      // console.log(this.depts[0])
+      this.queryParams.departmentId = this.depts[0].id
+      // 获取数组件实例，通过内置方法，根据参数(id)设置默认初始选中项
+      // el-tree为异步更新，需要在组件渲染完成后再调用内置方法
+      this.$nextTick(() => this.$refs.deptTree.setCurrentKey(this.queryParams.departmentId))
+    },
+    // 获取点击选中的节点数据
+    selsctNode(node) {
+      // console.log(e)
+      // 记录当前选中的节点id，用户后续接口获取数据
+      this.queryParams.departmentId = node.id
+    }
+  }
 }
 </script>
 
