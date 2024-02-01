6c61315401b9d276aae39583e54240445f14e587
hong <397956861@qq.com>
2024-02-01 14:36:57 +0800

    员工详情数据准备和表单校验

--- src/views/employee/detail.vue
+++ src/views/employee/detail.vue
@@ -2,12 +2,12 @@
   <div class="dashboard-container">
     <div class="app-container">
       <div class="edit-form">
-        <el-form ref="userForm" label-width="220px">
+        <el-form ref="userForm" :model="userInfo" :rules="rules" label-width="220px">
           <!-- 姓名 部门 -->
           <el-row>
             <el-col :span="12">
               <el-form-item label="姓名" prop="username">
-                <el-input size="mini" class="inputW" />
+                <el-input v-model="userInfo.username" size="mini" class="inputW" />
               </el-form-item>
             </el-col>
 
@@ -16,7 +16,8 @@
           <el-row>
             <el-col :span="12">
               <el-form-item label="工号" prop="workNumber">
-                <el-input size="mini" class="inputW" />
+                <!-- 工号由系统自动生成，无需输入，直接禁用 -->
+                <el-input v-model="userInfo.workNumber" disabled size="mini" class="inputW" />
               </el-form-item>
             </el-col>
           </el-row>
@@ -25,6 +26,7 @@
             <el-col :span="12">
               <el-form-item label="手机" prop="mobile">
                 <el-input
+                  v-model="userInfo.mobile"
                   size="mini"
                   class="inputW"
                 />
@@ -41,7 +43,10 @@
           <el-row>
             <el-col :span="12">
               <el-form-item label="聘用形式" prop="formOfEmployment">
-                <el-select size="mini" class="inputW" />
+                <el-select v-model="userInfo.formOfEmployment" placeholder="请选择聘用形式" size="mini" class="inputW">
+                  <el-option label="正式" :value="1" />
+                  <el-option label="非正式" :value="2" />
+                </el-select>
               </el-form-item>
             </el-col>
           </el-row>
@@ -49,9 +54,11 @@
             <el-col :span="12">
               <el-form-item label="入职时间" prop="timeOfEntry">
                 <el-date-picker
+                  v-model="userInfo.timeOfEntry"
                   size="mini"
                   type="date"
                   value-format="yyyy-MM-dd"
+                  placeholder="请选择入职时间"
                   class="inputW"
                 />
               </el-form-item>
@@ -59,8 +66,9 @@
           </el-row>
           <el-row>
             <el-col :span="12">
-              <el-form-item label="转正时间">
+              <el-form-item label="转正时间" prop="correctionTime">
                 <el-date-picker
+                  v-model="userInfo.correctionTime"
                   size="mini"
                   type="date"
                   class="inputW"
@@ -79,7 +87,7 @@
           <!-- 保存个人信息 -->
           <el-row type="flex">
             <el-col :span="12" style="margin-left:220px">
-              <el-button size="mini" type="primary">保存更新</el-button>
+              <el-button size="mini" type="primary" @click="saveData">保存更新</el-button>
             </el-col>
           </el-row>
         </el-form>
@@ -92,7 +100,51 @@
 <script>
 
 export default {
-
+  data() {
+    return {
+      userInfo: {
+        username: '', // 用户名
+        mobile: '', // 手机号
+        formOfEmployment: null, // 聘用形式
+        workNumber: '', // 工号
+        departmentId: null, // 部门id
+        timeOfEntry: '', // 入职时间
+        correctionTime: '' // 转正时间
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入员工姓名', trigger: 'blur' },
+          { min: 1, max: 4, message: '请输入1-4位姓名', trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '请输入员工手机号码', trigger: 'blur' },
+          { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号码', trigger: 'blur' }
+        ],
+        formOfEmployment: [{ required: true, message: '请选择员工聘用形式', trigger: 'blur' }],
+        departmentId: [{ required: true, message: '请选择员工部门', trigger: 'blur' }],
+        timeOfEntry: [{ required: true, message: '请选择员工入职时间', trigger: 'blur' }],
+        correctionTime: [
+          { required: true, message: '请选择员工转正时间', trigger: 'blur' },
+          {
+            validator: (rule, value, callback) => {
+              if (this.userInfo.timeOfEntry) {
+                if (new Date(this.userInfo.timeOfEntry) > new Date(value)) {
+                  callback(new Error('转正时间不能小于入职时间'))
+                  return
+                }
+              }
+              callback()
+            }
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    saveData() {
+      this.$refs.userForm.validate()
+    }
+  }
 }
 </script>
 
