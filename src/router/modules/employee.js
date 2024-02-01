41fdd22b104ff7e1925eb75217776e7dde47c7ea
hong <397956861@qq.com>
2024-02-01 13:23:22 +0800

    完成员工详情页面结构和路由配置

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -10,5 +10,12 @@ export default {
       title: '员工',
       icon: 'people'
     }
+  }, {
+    path: '/employee/detail',
+    component: () => import('@/views/employee/detail.vue'),
+    hidden: true, // 在左侧菜单中隐藏该二级路由
+    meta: {
+      title: '员工详情' // 页面上方导航显示的文本
+    }
   }]
 }
