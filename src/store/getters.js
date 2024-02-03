e801a0d1e855d64fa41acb63d2f88b98111e569f
hong <397956861@qq.com>
2024-02-03 20:42:30 +0800

    首页个人信息的显示

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,9 +2,11 @@ const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
   token: state => state.user.token,
-  userId: state => state.user.userInfo.suerId, // 用户ID
+  userId: state => state.user.userInfo.userId, // 用户ID
   avatar: state => state.user.userInfo.staffPhoto, // 用户头像
   name: state => state.user.userInfo.username, // 用户名称
-  routes: state => state.user.routes // 路由列表
+  routes: state => state.user.routes, // 路由列表
+  company: state => state.user.userInfo.company, // 公司名称
+  departmentName: state => state.user.userInfo.departmentName // 部门名称
 }
 export default getters
